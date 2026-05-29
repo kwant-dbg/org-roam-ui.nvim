ea8c106182cfda51a52b3a92be4991384b8a518c
kwant-dbg <harshit2003sharma@gmail.com>
2026-05-29 23:17:29 +0530

    Add graph and server coverage

--- tests/graph_spec.lua
+++ tests/graph_spec.lua
@@ -97,6 +97,115 @@ describe("org-roam-ui-nvim graph serialization", function()
     assert.matches('"olp":null', encoded, nil, true)
   end)
 
+  it("forwards refs, aliases, and NOTER_PAGE properties", function()
+    local node = graph.to_orui_node({
+      id = "paper",
+      file = "/tmp/paper.org",
+      title = "Paper",
+      level = 0,
+      refs = { "cite:key", "https://example.test" },
+      aliases = { "Alias One", "Alias Two" },
+      properties = {
+        NOTER_PAGE = 42,
+      },
+      tags = {},
+      linked = {},
+      range = { start = pos(0), end_ = pos(10) },
+    })
+
+    assert.are.same({
+      ROAM_REFS = "cite:key https://example.test",
+      ROAM_ALIASES = "Alias One Alias Two",
+      NOTER_PAGE = "42",
+    }, node.properties)
+  end)
+
+  it("keeps native refs and aliases ahead of duplicate node properties", function()
+    local node = graph.to_orui_node({
+      id = "a",
+      file = "/tmp/a.org",
+      title = "A",
+      level = 0,
+      refs = { "native-ref" },
+      aliases = { "Native Alias" },
+      properties = {
+        ROAM_REFS = "property-ref",
+        ROAM_ALIASES = "Property Alias",
+        NOTER_PAGE = "7",
+      },
+      tags = {},
+      linked = {},
+      range = { start = pos(0), end_ = pos(10) },
+    })
+
+    assert.are.same({
+      ROAM_REFS = "native-ref",
+      ROAM_ALIASES = "Native Alias",
+      NOTER_PAGE = "7",
+    }, node.properties)
+  end)
+
+  it("deduplicates and sorts links by source then target", function()
+    local links = graph.links_from_nodes({
+      z = {
+        id = "z",
+        linked = {
+          b = { pos(1), pos(2) },
+          a = { pos(3) },
+        },
+      },
+      a = {
+        id = "a",
+        linked = {
+          c = { pos(4) },
+          b = { pos(5) },
+        },
+      },
+      m = {
+        id = "m",
+        linked = {
+          a = { pos(6) },
+        },
+      },
+    })
+
+    assert.are.same({
+      { source = "a", target = "b", type = "id" },
+      { source = "a", target = "c", type = "id" },
+      { source = "m", target = "a", type = "id" },
+      { source = "z", target = "a", type = "id" },
+      { source = "z", target = "b", type = "id" },
+    }, links)
+  end)
+
+  it("reads org-roam.nvim databases through internal_sync when present", function()
+    local called = false
+    local database
+    database = {
+      internal_sync = function(self)
+        called = self == database
+        return {
+          __nodes = {
+            a = {
+              id = "a",
+              file = "/tmp/a.org",
+              title = "A",
+              tags = {},
+              level = 0,
+              linked = {},
+              range = { start = pos(0), end_ = pos(10) },
+            },
+          },
+        }
+      end,
+    }
+
+    local data = graph.from_database(database)
+
+    assert.is_true(called)
+    assert.are.equal("a", data.nodes[1].id)
+  end)
+
   it("computes nested outline paths from ancestor headings", function()
     local path = "/tmp/org-roam-ui-nvim-olp.org"
     local lines = {
